83321bf1f312a27a9d3da40b92bbf0ba94adae6e
Jill Yeh <j456711282@gmail.com>
2019-04-02 19:06:13 +0800

    Set up MVC.

---
+++ Reconds/Controller/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reconds
+//
+//  Created by 渡邊君 on 2019/4/1.
+//  Copyright © 2019 Yu-Hsin Yeh. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
