9dcb9a0635b350b0371218fd5ca24a80e3fd10c8
Jill Yeh <j456711282@gmail.com>
2019-04-03 14:45:51 +0800

    Revise MVC pattern and add function to HomeViewController's + icon.

--- Reconds/Controller/Home/HomeViewController.swift
+++ Reconds/Controller/Home/HomeViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBAction func addVideoButtonPressed(_ sender: UIBarButtonItem) {
+        
+        createProjectNameAlert()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    func createProjectNameAlert() {
+        
+        let alert = UIAlertController(title: "請輸入影片名稱", message: "命名後仍可更改。", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: { textField in
+            
+            textField.placeholder = "未命名"
+        })
+        
+        let confirmAction = UIAlertAction(title: "確定", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "取消", style: .default, handler: nil)
+        
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
