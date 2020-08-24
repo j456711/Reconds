80348f075edea600d728c238e56b37395e63f36b
YU HSIN YEH <j456711282@gmail.com>
2020-08-24 17:05:06 +0800

    refactor: Refactor JYProgressHUD.

--- Reconds/ThirdPartyWrapper/JGProgressHUDWrapper.swift
+++ Reconds/ThirdPartyWrapper/JGProgressHUDWrapper.swift
@@ -12,21 +12,24 @@ class JYProgressHUD {
     
     static let shared = JYProgressHUD()
     
-    let hud = JGProgressHUD(style: .dark)
+    private init() {}
     
-    func showSuccess(in view: UIView, with text: String = "儲存成功") {
+    private let hud = JGProgressHUD(style: .dark)
+}
+
+extension JYProgressHUD {
+    
+    func showSuccess(in view: UIView) {
         
-        hud.textLabel.text = text
-        
+        hud.textLabel.text = "儲存成功"
         hud.indicatorView = JGProgressHUDSuccessIndicatorView()
         
         hud.show(in: view)
     }
     
-    func showFailure(in view: UIView, with text: String = "儲存失敗") {
+    func showFailure(in view: UIView) {
         
-        hud.textLabel.text = text
-        
+        hud.textLabel.text = "儲存失敗"
         hud.indicatorView = JGProgressHUDErrorIndicatorView()
         
         hud.show(in: view)
@@ -35,7 +38,6 @@ class JYProgressHUD {
     func showIndeterminate(in view: UIView, with text: String = "輸出中") {
         
         hud.textLabel.text = text
-
         hud.indicatorView = JGProgressHUDIndeterminateIndicatorView()
         
         hud.show(in: view)
